a9915401901972516b196ba5b7684c371113c058
Olga Suvorova <olga.suv.ya@yandex.ru>
2023-01-27 15:06:07 +0300

    add tests

--- src/test/java/ru/netology/testmode/tests/TestModeTests.java
+++ src/test/java/ru/netology/testmode/tests/TestModeTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
 import static ru.netology.testmode.data.DataGenerator.Registration.getRegisteredUser;
+import static ru.netology.testmode.data.DataGenerator.Registration.getUser;
+import static ru.netology.testmode.data.DataGenerator.getRandomLogin;
 
 
 public class TestModeTests {
@@ -17,14 +19,64 @@ public class TestModeTests {
     }
 
     @Test
-    @DisplayName("Should successfuly login with active registered user")
+    @DisplayName("Should successfully login with active registered user")
     void ShouldSuccessfulLoginIfRegisteredActiveUser() {
         var registeredUser = getRegisteredUser("active");
         $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
         $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
         $$("span.button__text").find(Condition.exactText("Продолжить")).click();
-        $("h2").shouldHave(Condition.exactText("Личный кабинет")).shouldBe(Condition.visible);
+        $("h2").shouldHave(Condition.exactText("Личный кабинет"))
+                .shouldBe(Condition.visible);
     }
 
+    @Test
+    @DisplayName("Should get error message if login with not registered user")
+    void ShouldGetErrorIfNotRegisteredUser() {
+        var notRegisteredUser = getUser("active");
+        $("[data-test-id='login'] input").setValue(notRegisteredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(notRegisteredUser.getPassword());
+        $$("span.button__text").find(Condition.exactText("Продолжить")).click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"))
+                .shouldBe((Condition.visible));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with blocked registered user")
+    void ShouldGetErrorIfBlockedRegisteredUser() {
+        var blockedUser = getRegisteredUser("blocked");
+        $("[data-test-id='login'] input").setValue(blockedUser.getLogin());
+        $("[data-test-id='password'] input").setValue(blockedUser.getPassword());
+        $$("span.button__text").find(Condition.exactText("Продолжить")).click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.text("Ошибка! Пользователь заблокирован"))
+                .shouldBe((Condition.visible));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login is wrong login")
+    void ShouldGetErrorIfWrongLogin() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongLogin = getRandomLogin();
+        $("[data-test-id='login'] input").setValue(wrongLogin);
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $$("span.button__text").find(Condition.exactText("Продолжить")).click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"))
+                .shouldBe((Condition.visible));
+    }
+
+    @Test
+    @DisplayName("Should get error message if password is wrong pasword")
+    void ShouldGetErrorIfWrongPassword() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongPassword = getRandomLogin();
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(wrongPassword);
+        $$("span.button__text").find(Condition.exactText("Продолжить")).click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.text("Ошибка! Неверно указан логин или пароль"))
+                .shouldBe((Condition.visible));
+    }
 
 }
